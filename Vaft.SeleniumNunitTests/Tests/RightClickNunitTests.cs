667a2f6285088b1299cd8ad0aae2a3a5d50eabb2
Domas Puras <domas.puras@visma.com>
2018-02-16 23:22:51 +0200

    Nunit test for performing right click

---
+++ Vaft.SeleniumNunitTests/Tests/RightClickNunitTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Vaft.Framework.Core;
+using Vaft.Framework.Driver;
+using Vaft.Framework.Element;
+
+namespace Vaft.SeleniumNunitTests.Tests
+{
+    public class RightClickNunitTests : TestBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.VaftExt().OpenUrl("https://swisnl.github.io/jQuery-contextMenu/demo.html");
+        }
+
+        [Test]
+        public void ExpandContextMenu()
+        {
+            IWebElement button = Driver.FindElement(By.XPath("//span[text()='right click me']"));
+
+            Assert.IsFalse(IsContextMenuDisplayed());
+            button.AdvancedAction().RightClick();
+            Assert.IsTrue(IsContextMenuDisplayed());
+        }
+
+        private bool IsContextMenuDisplayed()
+        {
+            try
+            {
+                Driver.FindElement(By.Id("context-menu-layer"));
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
