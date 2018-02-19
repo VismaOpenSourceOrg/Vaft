ede6a5f84939f0ae6aa36b9eb0aaf05ae8cd8f74
Vilius Zukauskas <xxzukaus@visma.com>
2018-02-19 21:38:37 +0200

    start of xunit test project

---
+++ Vaft.Selenium.XUnit.Tests/Pages/Page.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using Vaft.Framework.Core;
+
+namespace Vaft.Selenium.XUnit.Tests.Pages
+{
+    public class Page : PageBase
+    {
+        public Page(IWebDriver driver) : base(driver)
+        {
+        }
+
+
+    }
+}
---
+++ Vaft.Selenium.XUnit.Tests/Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using Vaft.Framework.Core;
+using Xunit;
+
+namespace Vaft.Selenium.XUnit.Tests.Tests
+{
+    public class Test : XUnitTestBase
+    {
+
+        [Fact]
+        public void LoginTest()
+        {
+            
+        }
+    }
+}
