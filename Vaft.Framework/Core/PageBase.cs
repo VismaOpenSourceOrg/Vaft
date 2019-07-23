fec3f7fb3efe261870ffd6ea362f37826d724765
Domas Puras <domas.puras@visma.com>
2019-07-23 15:14:33 +0300

    Updated Chrome Driver version

--- Vaft.Framework/Core/PageBase.cs
+++ Vaft.Framework/Core/PageBase.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
-using Vaft.Framework.Settings;
 
 namespace Vaft.Framework.Core
 {
@@ -13,24 +11,6 @@ namespace Vaft.Framework.Core
             : base(driver)
         {
             PageFactory.InitElements(driver, this);
-            //PageFactory.InitElements(this, new RetryingElementLocator(driver, TimeSpan.FromSeconds(3)));
-        }
-
-        /// <summary>Opens ApplicationBaseUrl defined in app.config</summary>
-        [Obsolete("Please use WebDriver extension method: Driver.VaftExt().OpenApplicationBaseUrl()")]
-        public PageBase NavigateToUrl()
-        {
-            Driver.Navigate().GoToUrl(Config.Settings.RuntimeSettings.AppBaseUrl);
-            return this;
-        }
-
-        /// <summary>Opens ApplicationBaseUrl defined in app.config</summary>
-        /// <param name="urlPath">Dynamic part of URL path (excluding base URL).</param>
-        [Obsolete("Please use WebDriver extension method: Driver.VaftExt().OpenApplicationBaseUrl()")]
-        public PageBase NavigateToUrl(string urlPath)
-        {
-            Driver.Navigate().GoToUrl(Config.Settings.RuntimeSettings.AppBaseUrl + urlPath);
-            return this;
         }
     }
 }
